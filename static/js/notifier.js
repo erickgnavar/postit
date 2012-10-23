b6d4c9c55a375a56e36a75e8f6dae27d47b98185
Erick Navarro <erickxls@gmail.com>
2012-10-22 22:56:05 -0500

    init commit

---
+++ static/js/notifier.js
@@ -0,0 +1,30 @@
+var socket = io.connect('http://192.168.1.10:4000');
+
+socket.on('new-post', function (post){
+	addPost(post);
+});
+
+$(document).on('ready', function(){
+	$('#form-new-post').on('submit', function (e) {
+		e.preventDefault();
+		var title = $(this).find('#title').val();
+		var content = $(this).find('#content').val();
+		var post = {
+			title: title,
+			content: content,
+			author: 'Benito'
+		};
+		socket.emit('save-post', JSON.stringify(post));
+		return false;
+	});
+});
+
+var addPost = function (post) {
+	var txt = '<li class="postit">';
+	txt += '<h2 >' + post.title + '</h2>';
+	txt += '<p>' + post.content + '</p>';
+	txt += '<i>' + post.author + '</i>';
+	txt += '</li>';
+	$('#posts').prepend(txt);
+	$('#posts li:first-child').hide().fadeIn('slow');
+};
---
+++ web.js
@@ -0,0 +1,87 @@
+var express = require('express');
+var app = express();
+// var server = require('http').createServer(app);
+
+var io = require('socket.io').listen(4000, {log: false});
+
+var mongo = require('./mongo.js');
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+	console.log("Listening on " + port);
+});
+
+// var url = 'mongodb://admin:olimpo30@ds037407.mongolab.com:37407/pizarron';
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.use(express.bodyParser());
+app.use(express.compress());
+
+app.use('/static', express.static(__dirname + '/static'));
+
+io.sockets.on('connection', function (socket){
+	socket.on('save-post', function (data){
+		var post = JSON.parse(data);
+		mongo.savePost(post, function (success) {
+			if(success) {
+				io.sockets.emit('new-post', post);
+			}
+		});
+	});
+});
+
+
+app.get('/', function (request, response) {
+	mongo.getPosts(function (posts){
+		if(posts){
+			response.render('home', {posts: posts});
+		}
+		else
+			response.send('hola');
+	});
+});
+
+app.post('/login/', function (request, response){
+	var username = request.body.username;
+	var password = request.body.password;
+	var user = {
+		'username': username,
+		'password': password
+	};
+	mongo.login(user, function (success){
+		if(success){
+			response.send('login successfull');
+		}else{
+			response.send('problem with username or password');
+		}
+	});
+});
+
+app.get('/login', function (request, response) {
+	response.render('login');
+});
+
+app.get('/logout', function (request, response) {
+	response.redirect('/');
+});
+
+app.post('/post/', function (request, response) {
+	var post = {
+		'title' : request.body.title,
+		'author' : 'Erick Navarro',
+		'content' : request.body.content
+	};
+	mongo.savePost(post, function (success) {
+		if(success){
+			response.redirect('/');
+		}else{
+			response.send('BAD');
+		}
+	});
+});
+
+app.get('/post', function (request, response){
+	response.render('new_post');
+});
