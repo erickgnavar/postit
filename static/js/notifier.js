46bf6f9b77a9688925db5f217ae09428f5c12c23
Erick Navarro <erickxls@gmail.com>
2013-03-02 21:41:34 -0500

    added google analytics tracking code

--- static/js/notifier.js
+++ static/js/notifier.js
@@ -7,6 +7,16 @@ socket.on('drop post', function (pk) {
 	removePost(pk);
 });
 
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-38959581-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 $(document).on('ready', function(){
 	$('#save-post-btn').on('click', function (e) {
 		e.preventDefault();
